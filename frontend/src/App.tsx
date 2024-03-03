9126cdd31e5ac2d187253dada60a0980808ad4c2
Kevin Tan <tankevin793@gmail.com>
2024-03-02 21:11:52 -0500

    frontend solace kinda

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,42 @@
 import { useState } from 'react'
 import './App.css'
+import Paho from "paho-mqtt";
+// @ts-ignore
+import messaging from './solace/Messaging.js';
 
-function App() {
+function App(this: any) {
+  const [messages, setMessages] = useState([]);
+  const [connected, setConnected] = useState(false);
+  
+  const handleMessage = (message) => {
+    console.log(message);
+    setMessages(cur => cur.concat(message.payloadString));
+	}
+  messaging.register(handleMessage.bind(this));
+  const handleSendClick = () => {
+		const message = new Paho.Message(JSON.stringify({text: "Hello"}));
+		message.destinationName = "SomeTopic";
+		messaging.send(message);
+	}
+
+  const handleConnectClick = () => {
+		if (connected) {
+			messaging.disconnect();
+      setConnected(false);
+		} else {
+			messaging.connectWithPromise().then(response => {
+				console.log("Succesfully connected to Solace Cloud.", response);
+				messaging.subscribe("SomeTopic");
+        setConnected(true);
+			}).catch(error => {
+				console.log("Unable to establish connection with Solace Cloud, see above logs for more details.", error);
+			});
+		}
+	}
+
+
+  const sendButton = connected ? <button onClick={handleSendClick}>Send</button> : <button disabled>Send</button>;
+
 
   return (
     <div>
@@ -29,6 +64,17 @@ function App() {
           <p>Or sign up to start donating now!</p>
           <button>Sign Up</button>
         </div>
+      </section>
+      <section>
+          <div className="buttons">
+            <button onClick={handleConnectClick}>{connected ? 'Disconnect' : 'Connect'}</button>
+            {sendButton}
+          </div>
+          <ol>
+            {messages.map((message, index) => {
+              return <li key={index}>{message}</li>
+            })}
+          </ol>
       </section>
       <footer>
         <p>&copy; 2024 Leftover Goodness. All rights reserved.</p>
